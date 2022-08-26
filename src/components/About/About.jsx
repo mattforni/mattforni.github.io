80398f255bc0450d5922aaabd30117b95cf99abb
Matthew Fornaciari <mattforni@gmail.com>
2022-08-26 13:23:26 +1000

    Updated about content to be streamlined across a single row

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import './About.scss';
+import PortraitImage from "../../assets/images/sand-boarding.jpg";
 
 const HelloNameTag = () => {
   return (
@@ -20,21 +21,20 @@ const About = () => {
   const linkAmazon = <a href='https://amazon.com' target='_blank' class='LinkAmazon'>Amazon</a>;
   const linkGremlin = <a href='https://gremlin.com' target='_blank' class='LinkGremlin'>Gremlin</a>;
   const linkSalesforce = <a href='https://salesforce.com' target='_blank' class='LinkSalesforce'>Salesforce</a>;
-  const linkEdwardAbbey = <a href='https://www.goodreads.com/quotes/11895-one-final-paragraph-of-advice-do-not-burn-yourselves-out' target='_blank'>this quote</a>;
-  const linkFindForni = <a href='http://findforni.info/' target='_blank'>following my journey</a>;
-  const linkGoodbyeGremlin = <a href='https://medium.com/@callmeforni/goodbye-gremlin-hello-world-b119abd3d370' target='_blank'>read my thoughts</a>;
+  const linkFindForni = <a href='http://findforni.info/' target='_blank'>check out his Garmin live map</a>;
+  const linkGoodbyeGremlin = <a href='https://medium.com/@callmeforni/goodbye-gremlin-hello-world-b119abd3d370' target='_blank'>left Gremlin</a>;
 
   return (
     <div className='About'>
-      <HelloNameTag />
+      <img src={PortraitImage} alt='Portrait Image'/>
       <div className='AboutContent'>
-        <p>
-          Well ... kind of. My name is Matthew Fornaciari, but most everyone calls me Forni. I was born in the Bay Area, grew up in LA, studied in Providence, and have lived in Seattle, San Francisco, and now Denver, though I will be leaving the Denver area at the end of March 2022. I consider myself a reluctant technologist and digital minimalist, which means that I try to spend as much time away from screens as I do in front of them. It also means that {linkEdwardAbbey} has slowly but surely become my north star and I very much consider it words to live by. You can check out my Garmin live map if have any interest in {linkFindForni}.
-        </p>
-        <br/>
-        <p>
-          I am a technologist by trade and have done stints at {linkAmazon}, {linkSalesforce}, and {linkGremlin}, the latter of which I founded in 2016. I left Gremlin at the end of February 2022 and am currently taking some time to rest and digest that experience before I dive in to the next chapter of my professional life. If your interested in what that journey was like please feel free to {linkGoodbyeGremlin} on my time at Gremlin.
-        </p>
+        <HelloNameTag/>
+        <div className='AboutContentIntro'>
+          Matthew Fornaciari, better known as Forni, is equal parts outdoorsman and technologist. Born in the Bay Area, he grew up in LA, studied in Providence, and has lived in Seattle, San Francisco, and Denver, before landing in Hobart, TAS, Australia where he currently resides.
+        </div>
+        <div className='AboutContentExpansion'>
+          His professional specialty is helping companies build more reliable computer systems, which he has done at {linkAmazon}, {linkSalesforce}, and most recently a diverse array of Fortune 500 companies via {linkGremlin}, which he founded in January 2016. Matt {linkGoodbyeGremlin} at the end of February 2022 and is currently taking some time to rest and digest that experience while running, swimming, cooking, and generally exploring. You can {linkFindForni} to follow along on his latest adventures.
+        </div>
       </div>
     </div>
   );
